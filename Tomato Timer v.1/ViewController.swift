e2de4345b7e1ecd7f33d0167dda9ff6d9c0e85ec
aamer Essa <aamer.es12@gmail.com>
2022-12-19 17:08:52 +0300

    Initial Commit

---
+++ Tomato Timer v.1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Tomato Timer v.1
+//
+//  Created by Aamer Essa on 19/12/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Tomato Timer v.1UITests/Tomato_Timer_v_1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Tomato_Timer_v_1UITestsLaunchTests.swift
+//  Tomato Timer v.1UITests
+//
+//  Created by Aamer Essa on 19/12/2022.
+//
+
+import XCTest
+
+final class Tomato_Timer_v_1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
